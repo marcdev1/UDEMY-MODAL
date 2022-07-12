930c8732dcd7174ce20818930e7aafab6e8a97fe
Devienne Marc <mdevienne1@gmail.com>
2022-07-12 15:17:26 +0200

    Modal

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', function () {
+//     console.log('Bouton cliqué'); // Permet de verrifier, ds la console si les boutons fonctionnent
+//   });
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Bouton cliqué');
+    modal.classList.remove('hidden'); //le point n'est pas utilisé car c'est juste le nom de la classe qui est necessaire.
+    overlay.classList.remove('hidden');
+  });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
